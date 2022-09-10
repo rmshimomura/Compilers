3451f9b4a61deee29951e93cbe6801465960d7df
rmshimomura <shimomurarodrigo1@gmail.com>
2022-09-10 18:35:20 -0300

    Fixed

--- Listas/Lista_5/Ex_1/compiler.hpp
+++ Listas/Lista_5/Ex_1/compiler.hpp
@@ -5,106 +5,113 @@
 
 namespace Compiler {
 
-    class State {
+class State {
+    typedef struct transition {
+        char input;
+        State *state;
+    } transition;
 
-        typedef struct transition {
-            char input;
-            State* state;
-        } transition;
+   private:
+    std::vector<transition> transitions;
 
-    private:
+   public:
+    std::string name, type;
+    State(std::string name, std::string type) : name(name), type(type) {}
 
-        std::vector<transition> transitions;
+    void addTransition(char input, State &state) {
+        transitions.push_back({input, &state});
+    }
 
-    public:
-
-        std::string name, type;
-        State(std::string name, std::string type) : name(name), type(type) {}
-
-        void addTransition(char input, State &state) {
-            transitions.push_back({input, &state});
-        }
-
-        State* getNextState(char input) {
-            for (auto &transition : transitions) {
-                if (transition.input == input) {
-                    return transition.state;
-                } else if (input >= 'a' && input <= 'z' && transition.input == all_letters) {
-                    return transition.state;
-                } else if (input >= '0' && input <= '9' && transition.input == all_numbers) {
-                    return transition.state;
-                }
+    State *getNextState(char input) {
+        for (auto &transition : transitions) {
+            if (transition.input == input) {
+                return transition.state;
+            } else if (input >= 'a' && input <= 'z' && transition.input == all_letters) {
+                return transition.state;
+            } else if (input >= '0' && input <= '9' && transition.input == all_numbers) {
+                return transition.state;
             }
-            return NULL;
         }
-    };
+        return NULL;
+    }
+};
 
-    class Automata {
-    private:
-        std::vector<State> states;
-        State *initialState;
+class Automata {
+   private:
+    std::vector<State> states;
+    State *initialState;
 
-    public:
-        Automata(State &initialState) {
-            this->initialState = &initialState;
-            this->states.push_back(initialState);
-        }
-        void addState(State &state) {
-            this->states.push_back(state);
-        }
+   public:
+    Automata(State &initialState) {
+        this->initialState = &initialState;
+        this->states.push_back(initialState);
+    }
+    void addState(State &state) {
+        this->states.push_back(state);
+    }
 
-        void inputString(std::string &input) {
+    void inputString(std::string &input) {
+        State *currentState = this->initialState;
+        std::string currentToken = "";
+        std::string currentFinalToken = "";
 
-            State *currentState = this->initialState;
-            std::string currentToken = "";
-            std::string currentFinalToken = "";
+        int indexLastFinal = -1;
+        State *ptrLastFinal = NULL;
 
-            int indexLastFinal = -1;
-            State *ptrLastFinal = NULL;
-
-            for (int index = 0; index < input.length(); index++) {
-
-                if (input[index] == '\n') {
-                    std::cout << currentToken << std::endl;
-                    currentToken = "";
-                    continue;
+        for (int index = 0; index < input.length(); index++) {
+            if (input[index] == '\n') {
+                if (ptrLastFinal != NULL) {
+                    std::cout << currentFinalToken << std::endl;
+                    currentFinalToken = "";
+                    ptrLastFinal = NULL;
                 }
 
-                State* nextState = currentState->getNextState(input[index]);
+                currentFinalToken = "";
+                currentToken = "";
+                currentState = this->initialState;
+                ptrLastFinal = NULL;
+                indexLastFinal = -1;
+                continue;
+            }
 
-                if (nextState != NULL) {
-                    currentToken += input[index];
-                    currentState = nextState;
+            State *nextState = currentState->getNextState(input[index]);
+            
+            if (nextState != NULL) {
+                currentState = nextState;
+                currentToken += input[index];
+                if (currentState->type == "final") {
+                    indexLastFinal = index;
+                    ptrLastFinal = currentState;
+                    currentFinalToken = currentToken;
+                }
+
+                if (index == input.length() - 1) {
                     if (currentState->type == "final") {
-                        indexLastFinal = index;
-                        ptrLastFinal = currentState;
-                        currentFinalToken = currentToken;
+                        std::cout << currentFinalToken << std::endl;
+                        currentFinalToken = "";
+                        ptrLastFinal = NULL;
                     }
 
-                    if(index == input.length() - 1) {
-                        if (currentState->type == "final") {
-                            std::cout << currentToken << std::endl;
-                        }
                     break;
                 }
 
+            } else {
+
+                if (indexLastFinal != -1) {
+                    std::cout << currentFinalToken << std::endl;
+                    index = indexLastFinal;
+                    currentState = this->initialState;
+                    currentToken = "";
+                    indexLastFinal = -1;
+                    ptrLastFinal = NULL;
+                    currentFinalToken = "";
                 } else {
-                    if (ptrLastFinal != NULL) {
-                        std::cout << currentFinalToken << std::endl;
-                        index = indexLastFinal;
-                        currentToken = "";
-                        currentState = this->initialState;
-                        ptrLastFinal = NULL;
-                    } else {
-                        std::cout << "ERRO" << std::endl;
-                        currentToken = "";
-                        currentState = this->initialState;
-                    }
+                    std::cout << "ERRO" << std::endl;
+                    currentState = this->initialState;
+                    currentToken = "";
                 }
-                
             }
-
         }
-    };
-
+    }
+};
 }  // namespace Compiler
