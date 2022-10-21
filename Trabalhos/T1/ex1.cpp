47b43dbd9b99d73315aa9b35f7204192ff926f8f
rmshimomura <shimomurarodrigo1@gmail.com>
2022-10-21 00:46:19 -0300

    Tokens recognized, need syntactic part

---
+++ Trabalhos/T1/ex1.cpp
@@ -0,0 +1,29 @@
+#include "LETSGO.hpp"
+
+std::vector<std::string> transform_input() {
+    std::vector<std::string> strings;
+    std::string line;
+    while (std::getline(std::cin, line)) {
+        strings.push_back(line);
+    }
+    return strings;
+}
+
+int main () {
+
+    Compiler::Lexic_Analyzer lexic;
+
+    std::vector<std::string> strings = transform_input();
+    
+    for(int i = 0; i < strings.size(); i++) {
+        lexic.lexic_analyze(strings[i]);
+    }
+
+    Compiler::Syntactic_analyzer syntactic;
+
+    // syntactic.syntax_analyze();
+
+    syntactic.results();
+
+
+}
